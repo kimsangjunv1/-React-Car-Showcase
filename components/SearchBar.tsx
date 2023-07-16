29f69ede22206c814823bad8c3b2ee4571fb299c
kimsangjunv1 <ingyoe64@gmail.com>
2023-07-16 15:27:18 +0900

    메인 검색 버튼 추가

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,24 @@
 "use client";
+import Image from "next/image";
+
 import { useState } from "react";
 import { SearchManufacturer } from "./";
 
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
+
 const SearchBar = () => {
   const [manuFacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
   const handleSearch = () => {};
 
@@ -14,7 +29,27 @@ const SearchBar = () => {
           manuFacturer={manuFacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px]"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
