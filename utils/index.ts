6e18a2b9a421ef64c3f3a3461535f91440d2d47f
kimsangjunv1 <ingyoe64@gmail.com>
2023-07-17 00:51:18 +0900

    Improve: 조건 별 검색 기능 구현

--- utils/index.ts
+++ utils/index.ts
@@ -36,6 +36,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
+
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
 
